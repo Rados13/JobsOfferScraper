b59c4852a981bbdfcf579b31c639a8d036dad7d6
Rados13 <radoslawszuma@gmail.com>
2020-10-17 22:37:29 +0200

    Fixing another bug from sqlite

--- sql_app/database.py
+++ sql_app/database.py
@@ -8,7 +8,8 @@ import os
 SQLALCHEMY_DATABASE_URL = os.environ.get("DATABASE_URL")
 
 engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+    SQLALCHEMY_DATABASE_URL
+    # only for sqlite , connect_args={"check_same_thread": False}
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
