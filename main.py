ec90f74f9f8d17e989e7de60d43ede8169ecdd5b
Rados13 <radoslawszuma@gmail.com>
2020-10-26 17:41:19 +0100

    Little fixes

--- main.py
+++ main.py
@@ -33,21 +33,19 @@ def new_last_scraped_date() -> Dict:
     return {"last_scraped": date.today()}
 
 
-def start_last_scraped(db: Session = get_session_to_start()):
-    if crud.get_last_scraped_date(db) is None:
+def start_last_scraped(db: Session = get_session_to_start(), scrap_again: bool = False):
+    db_date = crud.get_last_scraped_date(db)
+    must_scrap = False
+    if db_date is None:
         crud.create_last_scraped_date(db, schemas.LastScrapedCreate(**new_last_scraped_date()))
+        must_scrap = True
+    else:
+        db_date = db_date.last_scraped
+
+    if must_scrap or scrap_again or db_date != date.today():
         thread = Thread(target=new_thread_check_updates, args=(db,))
         thread.start()
-        print("Start server")
-
-
-def update_last_scraped(db: Session, scrap_again: bool = False):
-    db_date = crud.get_last_scraped_date(db).last_scraped
-    if db_date != date.today() or scrap_again:
-        crud.update_last_scraped_date(db, new_last_scraped_date())
-        thread = Thread(target=new_thread_check_updates, args=(db,))
-        thread.start()
-        print("End update")
+        print("Start scrap")
 
 
 def new_thread_check_updates(db: Session):
@@ -65,7 +63,7 @@ async def root(db: Session = Depends(get_db)):
 
 @app.get("/offers/{website}")
 def read_offers_by_website(website: str, skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    update_last_scraped(db)
+    start_last_scraped(db)
     for website_enum in WebsiteName:
         if website_enum.value == website:
             return crud.get_offers_by_website_name(db, website_enum, skip=skip, limit=limit)
@@ -75,7 +73,7 @@ def read_offers_by_website(website: str, skip: int = 0, limit: int = 100, db: Se
 
 @app.get("/offers/offer/{offer_id}")
 def read_offer(offer_id: int, db: Session = Depends(get_db)):
-    update_last_scraped(db)
+    start_last_scraped(db)
     db_offer = crud.get_offer(db, offer_id=offer_id)
     if db_offer is None:
         raise HTTPException(status_code=404, detail="This offer doesn't exist")
@@ -84,13 +82,13 @@ def read_offer(offer_id: int, db: Session = Depends(get_db)):
 
 @app.get("/offers")
 def read_offers(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
-    update_last_scraped(db)
+    start_last_scraped(db)
     return crud.get_offers(db, skip=skip, limit=limit)
 
 
 """
 @app.get("/scrap/linkedin")
 async def scrap_linkedin(db: Session = Depends(get_db)):
-    update_last_scraped(db,True)
+    start_last_scraped(db,True)
     return {"work": "Start scrap linkedin"}
 """
